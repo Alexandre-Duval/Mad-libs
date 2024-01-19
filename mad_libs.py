55a9878366e0a7561e0cb81a6fc4121aada1efb0
Alexandre Duval <alexsantosduval@outlook.com>
2024-01-19 17:23:50 +0000

    fixed soe problems

--- mad_libs.py
+++ mad_libs.py
@@ -1,22 +1,39 @@
+import os
+
+# words that the player will be asked to input
 words = ["time of day", "place", "adjective", "animal", "noun", "adjective", "toy", "adjective"]
 
-mad_lib = f"{words[0]}, I went to the {words[1]} and found a {words[2]} {words[3]} hiding behind a {words[4]}. It was so {words[5]} that I couldn't resist taking it home. We spent the day together, playing with {words[6]} and laughing until our stomachs hurt. It was the most {words[7]} day ever!"
+# list that will contain the answers the user inputs.
+answers = []
 
-# Takes in user input for every word in the words list and prints the completed text. 
-def user_input(words: list):
-    input("Welcome to the mad libs program!\n")
-    for word in words:
-        while True:
-            answers = input(f"{word}: ")
-            if type(answers) == str:
-                word = answers
-                break
-            else: 
-                continue
-    print(mad_lib)
+# clears the terminal to make game more readable
+def clear():
+    os.system("cls" if os.name == "nt" else "clear")
 
-# Main function of program
+# Function that welcomes player to mad lib game and produces the text after taking user input for each word.
+def mad_lib():
+    clear()
+    while True:
+        question = input("Welcome to Mad Libs! Would you like to play (y/n): ")
+        if question == "y":
+            for i in range(len(words)):
+                user_input = input(f"{words[i]}: ")
+                answers.append(user_input)
+            template = f"{answers[0]}, I went to the {answers[1]} and found a {answers[2]} {answers[3]} hiding behind a {answers[4]}. It was so {answers[5]} that I couldn't resist taking it home. We spent the day together, playing with {answers[6]} and laughing until our stomachs hurt. It was the most {answers[7]} day ever!"
+            print(template)
+            print("\nThanks for playing!")
+            break
+        
+        elif question == "n":
+            print("Sorry to see you go.")
+            break
+
+        else:
+            print("Invalid input.")
+
+
+# Main funtion for mad lib game.
 def main():
-    user_input(words)
+    mad_lib()
 
 main()
